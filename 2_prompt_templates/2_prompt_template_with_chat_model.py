617fe31676a174e5bcdc8d9ced803f2d8a99d207
Fardin <faa35@sfu.ca>
2024-12-29 18:36:12 -0800

    2.2 notes and comments added

--- 2_prompt_templates/2_prompt_template_with_chat_model.py
+++ 2_prompt_templates/2_prompt_template_with_chat_model.py
@@ -1,12 +1,35 @@
 from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain_google_genai import GoogleGenerativeAI
+from pydantic import SecretStr
+import os
+
 
 # Load environment variables from .env
 load_dotenv()
 
+# # Create a ChatOpenAI model
+# model = ChatOpenAI(model="gpt-4o")
+
+
+# Convert API key to SecretStr
+api_key = os.getenv("GOOGLE_API_KEY")
+if api_key is None:
+    raise ValueError("GOOGLE_API_KEY is not set in the environment variables.")
+    
+secret_api_key = SecretStr(api_key)
+
+
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+model = GoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    api_key=secret_api_key  # Use SecretStr
+)
 
 # PART 1: Create a ChatPromptTemplate using a template string
 print("-----Prompt from Template-----")
@@ -15,7 +38,7 @@ prompt_template = ChatPromptTemplate.from_template(template)
 
 prompt = prompt_template.invoke({"topic": "cats"})
 result = model.invoke(prompt)
-print(result.content)
+print(result)
 
 # PART 2: Prompt with Multiple Placeholders
 print("\n----- Prompt with Multiple Placeholders -----\n")
@@ -26,7 +49,7 @@ prompt_multiple = ChatPromptTemplate.from_template(template_multiple)
 prompt = prompt_multiple.invoke({"adjective": "funny", "animal": "panda"})
 
 result = model.invoke(prompt)
-print(result.content)
+print(result)
 
 # PART 3: Prompt with System and Human Messages (Using Tuples)
 print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
@@ -37,4 +60,4 @@ messages = [
 prompt_template = ChatPromptTemplate.from_messages(messages)
 prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
 result = model.invoke(prompt)
-print(result.content)
+print(result)

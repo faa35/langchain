aa2e1e5462dccd0478ad8073a96597bdd9563071
Fardin <faa35@sfu.ca>
2024-12-30 00:35:58 -0800

    3.3 notes and comments added

--- 3_chains/3_chains_extended.py
+++ 3_chains/3_chains_extended.py
@@ -2,13 +2,35 @@ from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnableLambda
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain_google_genai import GoogleGenerativeAI
+from pydantic import SecretStr
+import os
 
 # Load environment variables from .env
 load_dotenv()
 
+# # Create a ChatOpenAI model
+# model = ChatOpenAI(model="gpt-4o")
+
+# Convert API key to SecretStr
+api_key = os.getenv("GOOGLE_API_KEY")
+if api_key is None:
+    raise ValueError("GOOGLE_API_KEY is not set in the environment variables.")
+    
+secret_api_key = SecretStr(api_key)
+
+
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+model = GoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    api_key=secret_api_key  # Use SecretStr
+)
+
 
 # Define prompt templates
 prompt_template = ChatPromptTemplate.from_messages(
@@ -19,8 +41,8 @@ prompt_template = ChatPromptTemplate.from_messages(
 )
 
 # Define additional processing steps using RunnableLambda
-uppercase_output = RunnableLambda(lambda x: x.upper())
-count_words = RunnableLambda(lambda x: f"Word count: {len(x.split())}\n{x}")
+uppercase_output = RunnableLambda(lambda x: x.upper())                        # upper() shows problem because of GoogleGenerativeAI
+count_words = RunnableLambda(lambda x: f"Word count: {len(x.split())}\n{x}")  # split() shows problem because of GoogleGenerativeAI
 
 # Create the combined chain using LangChain Expression Language (LCEL)
 chain = prompt_template | model | StrOutputParser() | uppercase_output | count_words

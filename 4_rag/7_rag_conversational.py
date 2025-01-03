3e1351ddcf0e644fc5901ac34eabdb2c5394b8a5
Fardin <faa35@sfu.ca>
2025-01-03 13:25:32 -0800

    4.7 notes and comments added

--- 4_rag/7_rag_conversational.py
+++ 4_rag/7_rag_conversational.py
@@ -6,7 +6,13 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.vectorstores import Chroma
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+# from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
+
+from langchain_google_genai import GoogleGenerativeAI
+from pydantic import SecretStr
+import os
+
 
 # Load environment variables from .env
 load_dotenv()
@@ -15,11 +21,13 @@ load_dotenv()
 current_dir = os.path.dirname(os.path.abspath(__file__))
 persistent_directory = os.path.join(current_dir, "db", "chroma_db_with_metadata")
 
-# Define the embedding model
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+# # Define the embedding model
+# embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")  # Choose your model
+
 
 # Load the existing vector store with the embedding function
-db = Chroma(persist_directory=persistent_directory, embedding_function=embeddings)
+db = Chroma(persist_directory=persistent_directory, embedding_function=embedding_function)
 
 # Create a retriever for querying the vector store
 # `search_type` specifies the type of search (e.g., similarity)
@@ -29,8 +37,29 @@ retriever = db.as_retriever(
     search_kwargs={"k": 3},
 )
 
+# # Create a ChatOpenAI model
+# llm = ChatOpenAI(model="gpt-4o")
+# Convert API key to SecretStr
+api_key = os.getenv("GOOGLE_API_KEY")
+if api_key is None:
+    raise ValueError("GOOGLE_API_KEY is not set in the environment variables.")
+    
+secret_api_key = SecretStr(api_key)
+
+
 # Create a ChatOpenAI model
-llm = ChatOpenAI(model="gpt-4o")
+llm = GoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    api_key=secret_api_key  # Use SecretStr
+)
+
+
+
+
 
 # Contextualize question prompt
 # This system prompt helps the AI understand that it should reformulate the question
